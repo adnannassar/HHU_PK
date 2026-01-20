d16d6161e946dff3890ed0b7d4c45e773a7d22d4
Adnan Nassar <adnan.nassar@materna.group>
2026-01-20 21:00:12 +0100

    added binary search tree part2

---
+++ src/BinarySearchTree/BinarySearchTree.java
@@ -0,0 +1,173 @@
+package BinarySearchTree;
+
+public class BinarySearchTree {
+    Node root;
+
+    public boolean isEmpty() {
+        if (root == null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void add(int value) {
+        if (isEmpty()) {
+            root = new Node(value, null, null);
+        } else {
+            add(value, root);
+        }
+    }
+
+    private void add(int value, Node node) {
+        if (value == node.value) {
+            return;
+        }
+        if (value < node.value) {
+            if (node.left == null) {
+                node.left = new Node(value, null, null);
+            } else {
+                add(value, node.left);
+            }
+
+        } else {
+            if (node.right == null) {
+                node.right = new Node(value, null, null);
+            } else {
+                add(value, node.right);
+            }
+        }
+    }
+
+
+    public String traverseInOrder() {
+        if (isEmpty()) {
+            return "";
+        } else {
+            return traverseInOrder(root);
+        }
+    }
+
+    // IN ORDER --> L W R wobei (L: Links, W: Wurzel, R: Rechts)
+    private String traverseInOrder(Node node) {
+        String result = "";
+
+        // LEFT
+        if (node.left != null) {
+            result += traverseInOrder(node.left);
+        }
+
+        // ROOT
+        result += node.value + " ";
+
+        // Right
+        if (node.right != null) {
+            result += traverseInOrder(node.right);
+        }
+
+        return result;
+    }
+
+    public String traversePreOrder() {
+        if (isEmpty()) {
+            return "";
+        } else {
+            return traversePreOrder(root);
+        }
+    }
+
+    // PRE ORDER --> W L R wobei (L: Links, W: Wurzel, R: Rechts)
+    private String traversePreOrder(Node node) {
+        String result = "";
+
+        // ROOT
+        result += node.value + " ";
+
+        // LEFT
+        if (node.left != null) {
+            result += traversePreOrder(node.left);
+        }
+
+        // Right
+        if (node.right != null) {
+            result += traversePreOrder(node.right);
+        }
+
+        return result;
+    }
+
+
+    public String traversePostOrder() {
+        if (isEmpty()) {
+            return "";
+        } else {
+            return traversePostOrder(root);
+        }
+    }
+
+    // PRE ORDER --> L R W wobei (L: Links, W: Wurzel, R: Rechts)
+    private String traversePostOrder(Node node) {
+        String result = "";
+
+        // LEFT
+        if (node.left != null) {
+            result += traversePostOrder(node.left);
+        }
+
+        // RIGHT
+        if (node.left != null) {
+            result += traversePostOrder(node.right);
+        }
+
+        // ROOT
+        result += node.value + " ";
+
+        return result;
+    }
+
+    public boolean search(int value) {
+        if (isEmpty()) {
+            return false;
+        } else {
+            return search(value, root);
+        }
+    }
+
+    private boolean search(int value, Node node) {
+        if (value == node.value) {
+            return true;
+        }
+
+        if (value < node.value) {
+            if (node.left != null) {
+                return search(value, node.left);
+            }
+        }
+
+        if (value > node.value) {
+            if (node.right != null) {
+                return search(value, node.right);
+            }
+        }
+
+        return false;
+    }
+
+    public boolean searchIterative(int value) {
+        if (isEmpty()) {
+            return false;
+        } else {
+            Node ptr = root;
+            while (ptr != null) {
+                if (ptr.value == value) {
+                    return true;
+                } else if (value < ptr.value) {
+                    ptr = ptr.left;
+                } else {
+                    ptr = ptr.right;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ src/BinarySearchTree/Main.java
@@ -0,0 +1,49 @@
+package BinarySearchTree;
+
+public class Main {
+    static BinarySearchTree bst = new BinarySearchTree();
+    public static void main(String[] args) {
+        bst.add(10);
+        bst.add(5);
+        bst.add(15);
+        bst.add(1);
+        bst.add(8);
+        bst.add(12);
+        bst.add(20);
+
+        System.out.println("Traverse In   Order: " + bst.traverseInOrder());
+        System.out.println("Traverse Pre  Order: " + bst.traversePreOrder());
+        System.out.println("Traverse Post Order: " + bst.traversePostOrder());
+
+        printSearchResultRecursiveCall(10);
+        printSearchResultRecursiveCall(20);
+        printSearchResultRecursiveCall(1);
+        printSearchResultRecursiveCall(30);
+
+        System.out.println();
+
+        printSearchResultIterativeCall(10);
+        printSearchResultIterativeCall(20);
+        printSearchResultIterativeCall(1);
+        printSearchResultIterativeCall(30);
+
+    }
+
+    private static void printSearchResultRecursiveCall(int nr) {
+        boolean nrFound = bst.search(nr);
+        if (nrFound) {
+            System.out.println(nr + " wurde im Baum gefunden! (Recursive)");
+        }else{
+            System.out.println(nr + " wurde im Baum nicht gefunden! (Recursive)");
+        }
+    }
+
+    private static void printSearchResultIterativeCall(int nr) {
+        boolean nrFound = bst.searchIterative(nr);
+        if (nrFound) {
+            System.out.println(nr + " wurde im Baum gefunden! (Iterative)");
+        }else{
+            System.out.println(nr + " wurde im Baum nicht gefunden! (Iterative)");
+        }
+    }
+}
